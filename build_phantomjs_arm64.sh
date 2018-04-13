8ebb2b200b148860b582e58b78b289ac24f1019b
Ian Johnson <ian.johnson@canonical.com>
2018-04-13 08:24:11 -0500

    update phantomjs build script
    * use git version 2.1.1
    * echo all commands as they are run
    * use a variable for keeping track of directories
    * make script executable standalone with hashbang
    * use whatever version of icu gets checkout that is 5X.X (currently it's 55.1)

--- build_phantomjs_arm64.sh
+++ build_phantomjs_arm64.sh
@@ -1,12 +1,15 @@
+#!/bin/bash
+set -x
 wget -c http://releases.llvm.org/3.8.0/clang+llvm-3.8.0-aarch64-linux-gnu.tar.xz
 tar -xf clang+llvm-3.8.0-aarch64-linux-gnu.tar.xz
 export CLANG_DIR=$(pwd)/clang+llvm-3.8.0-aarch64-linux-gnu/bin/
 git clone https://github.com/ariya/phantomjs.git
 cd phantomjs
-git checkout 2.1.3
+export PHANTOMJSDIR=$(pwd)
+git checkout 2.1.1
 git submodule init
 git submodule update
-sudo sed -i~orig -e 's/# deb-src/deb-src/' /etc/apt/sources.list 
+sudo sed -i~orig -e 's/# deb-src/deb-src/' /etc/apt/sources.list
 sudo apt-get update
 apt-get source openssl
 cd openssl-1.0.*
@@ -14,12 +17,13 @@ cd openssl-1.0.*
 make -j4 depend
 make -j4
 sudo make install_sw
-cd ..
+cd $PHANTOMJSDIR
 apt-get source icu
-cd icu-52.1/source
+cd icu-5*/source
 ./configure --prefix=/usr --enable-static --disable-shared
 make -j4
 sudo make install
+cd $PHANTOMJSDIR
 export CC=$CLANG_DIR/clang
-export CXX=$CLANG_DIR/clang++ 
+export CXX=$CLANG_DIR/clang++
 python build.py  --confirm --release --qt-config="-no-pkg-config" --git-clean-qtbase --git-clean-qtwebkit
